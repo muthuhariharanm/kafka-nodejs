d12dcef313b09148967cc8d0ded0c74be36aae4f
Muthu Hariharan M <37234273+muthuhariharanm@users.noreply.github.com>
2020-06-30 10:48:31 +0530

    Update config.js

--- producer/config.js
+++ producer/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   kafka: {
     TOPIC: 'reviews',
-    BROKERS: ['35.197.65.236:9092'],
-    GROUPID: 'bills-consumer-group',
+    BROKERS: ['<kafka-service-ip:port>'],
+    GROUPID: 'reviews-consumer-group',
     CLIENTID: 'sample-kafka-client'
   }
 }
